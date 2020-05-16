7195c83e11a9bf5120288cb2fde67eff1de289ca
Akshay Nanda <akshayn@sfu.ca>
2020-05-16 14:50:23 -0700

    Added Product Description Screen styling and used redux to set state after pressing add to card button

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -4,15 +4,22 @@ import { createStackNavigator } from "react-navigation-stack";
 import { Platform } from "react-native";
 import Colors from "../constants/Colors";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
-
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 const ProductsNavigator = createStackNavigator(
   {
-    ProductOverview: ProductsOverviewScreen
+    ProductOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primary : ""
+      },
+      headerTitleStyle: () => {
+        fontFamily: "open-sans-bold";
+      },
+      headerBackTitleStyle: () => {
+        fontFamily: "open-sans";
       },
       headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
     }
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ProductItem from "../../components/shop/ProductItem";
 import { FlatList, Text } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import * as cartActions from "../../store/actions/cart";
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
   return (
     <FlatList
       data={products}
@@ -13,8 +15,15 @@ const ProductsOverviewScreen = props => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewDetail={() => {}}
-          onAddToCart={() => {}}
+          onViewDetail={() => {
+            props.navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            });
+          }}
+          onAddToCart={() => {
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
         />
       )}
     />
