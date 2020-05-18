994da21ef412bc14236eedcf10bb84bf30476304
Akshay Nanda <akshayn@sfu.ca>
2020-05-18 12:35:54 -0700

    Authentication using authscreen and firebase ready

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,17 +1,88 @@
-import React from "react";
+import React, { useState, useReducer, useCallback } from "react";
 import {
   ScrollView,
   View,
-  Text,
   KeyboardAvoidingView,
   StyleSheet,
   Button
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useDispatch } from "react-redux";
+
 import Input from "../../components/UI/Input";
 import Card from "../../components/UI/Card";
 import Colors from "../../constants/Colors";
+import * as authActions from "../../store/actions/auth";
+
+const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE";
+
+const formReducer = (state, action) => {
+  if (action.type === FORM_INPUT_UPDATE) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.input]: action.value
+    };
+    const updatedValidities = {
+      ...state.inputValidities,
+      [action.input]: action.isValid
+    };
+    let updatedFormIsValid = true;
+    for (const key in updatedValidities) {
+      updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+    }
+    return {
+      formIsValid: updatedFormIsValid,
+      inputValidities: updatedValidities,
+      inputValues: updatedValues
+    };
+  }
+  return state;
+};
+
 const AuthScreen = props => {
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: "",
+      password: ""
+    },
+    inputValidities: {
+      email: false,
+      password: false
+    },
+    formIsValid: false
+  });
+
+  const authHandler = () => {
+    let actions;
+    if (isSignup) {
+      actions = authActions.signup(
+        formState.inputValues.email,
+        formState.inputValues.password
+      );
+    } else {
+      actions = authActions.login(
+        formState.inputValues.email,
+        formState.inputValues.password
+      );
+    }
+    dispatch(actions);
+  };
+
+  const inputChangeHandler = useCallback(
+    (inputIdentifier, inputValue, inputValidity) => {
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE,
+        value: inputValue,
+        isValid: inputValidity,
+        input: inputIdentifier
+      });
+    },
+    [dispatchFormState]
+  );
+
   return (
     <KeyboardAvoidingView
       behavior="padding"
@@ -23,15 +94,15 @@ const AuthScreen = props => {
           <ScrollView>
             <Input
               id="email"
-              label="Email"
+              label="E-Mail"
               keyboardType="email-address"
               required
               email
               autoCapitalize="none"
-              errorMessage="PLease enter a valid email address"
-              onInputChange={() => {}}
+              errorText="Please enter a valid email address."
+              onInputChange={inputChangeHandler}
               initialValue=""
-            ></Input>
+            />
             <Input
               id="password"
               label="Password"
@@ -40,23 +111,25 @@ const AuthScreen = props => {
               required
               minLength={5}
               autoCapitalize="none"
-              errorMessage="PLease enter a valid password"
-              onInputChange={() => {}}
+              errorText="Please enter a valid password."
+              onInputChange={inputChangeHandler}
               initialValue=""
-            ></Input>
+            />
             <View style={styles.buttonContainer}>
               <Button
-                title="Login"
+                title={isSignup ? "Sign Up" : "Login"}
                 color={Colors.primary}
-                onPress={() => {}}
-              ></Button>
+                onPress={authHandler}
+              />
             </View>
             <View style={styles.buttonContainer}>
               <Button
-                title="SignUp"
+                title={`Switch to ${isSignup ? "Login" : "Sign Up"}`}
                 color={Colors.accent}
-                onPress={() => {}}
-              ></Button>
+                onPress={() => {
+                  setIsSignup(prevState => !prevState);
+                }}
+              />
             </View>
           </ScrollView>
         </Card>
@@ -73,20 +146,20 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1
   },
+  gradient: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
   authContainer: {
     width: "80%",
     maxWidth: 400,
     maxHeight: 400,
     padding: 20
   },
-  gradient: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center"
-  },
   buttonContainer: {
     marginTop: 10
   }
 });
+
 export default AuthScreen;
